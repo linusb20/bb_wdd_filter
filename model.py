a2ddb5add35bce69c828ea0ecafef92c5a7a659b
Linus Buddrus <buddruslinus@gmail.com>
2023-05-30 17:26:21 +0200

    add padding to conv layers

--- model.py
+++ model.py
@@ -5,21 +5,22 @@ class WDDModel(torch.nn.Module):
         super().__init__()
 
         self.features = torch.nn.Sequential(
-            torch.nn.Conv3d(1,8, kernel_size=5),
+            torch.nn.Conv3d(1, 8, kernel_size=5, padding="same"),
             torch.nn.BatchNorm3d(8),
             torch.nn.ReLU(),
             torch.nn.MaxPool3d(2),
-            torch.nn.Conv3d(8,16, kernel_size=5),
+            torch.nn.Conv3d(8, 16, kernel_size=5, padding="same"),
             torch.nn.BatchNorm3d(16),
             torch.nn.ReLU(),
             torch.nn.MaxPool3d(2),
-            torch.nn.Conv3d(16,32, kernel_size=5),
+            torch.nn.Conv3d(16, 32, kernel_size=5, padding="same"),
             torch.nn.BatchNorm3d(32),
             torch.nn.ReLU(),
             torch.nn.MaxPool3d(2),
         )
+
         self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(32 * 6 * 10 * 10, 128),
+            torch.nn.Linear(32 * 10 * 13 * 13, 128),
             torch.nn.ReLU(),
             torch.nn.Dropout(),
             torch.nn.Linear(128, num_classes),
